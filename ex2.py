d3306b125041040f5880eaaaf6b01f6245ee2b60
yannian <motanchenyuan@126.com>
2018-10-25 19:03:13 +0800

    2018.10.25练习

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can reader your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this .") #and the comment after is ignored
+
+#you can also use comment to disable" or comment out code:
+# Print("This won't run.")
+
+print("This will run .")
